5ae86bbe6bfc6e32d7fb36b8e2bbecfa9c09d4d2
cideg-dev <cideg-dev@users.noreply.github.com>
2025-10-14 00:34:35 +0000

    deploy: f7201785bbb5454558d256b115c1c803f15f70f6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "701880925"
+    serviceWorkerVersion: "4221865558"
   }
 });
