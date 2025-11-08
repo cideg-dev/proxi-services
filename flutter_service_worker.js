f1d812ab9299d3121e212ad85b2b3be5dccb62b0
cideg-dev <cideg-dev@users.noreply.github.com>
2025-11-08 14:41:35 +0000

    deploy: 4ea5b5d2eae80ded4b962246c815afb00c7fe1c6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "620a3e4f56c83185ade20b1b918585a2",
-"flutter_bootstrap.js": "e7f4d6d546e89d7ca5e2bbe4274ee661",
+"flutter_bootstrap.js": "e4beb949a69dcf9a991c4d4ff1adf741",
 "version.json": "41d6c5261eae8e8fc247a1521d0f2010",
 "main.dart.js": "2384be2f5f0115b6fda3965f11e2783a",
 "landing_page.html": "40b2ab43679acb890795aacd95fc4f36"};
