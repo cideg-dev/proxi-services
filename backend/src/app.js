29a5bb6699b36ee669d920fd26e84494e267b712
cideg-dev <cideg.p.line@gmail.com>
2025-11-10 10:54:23 +0100

    fix: Ajout de l'import manquant pour cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
