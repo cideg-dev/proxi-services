97c2860721f658483e890c6aae98837325d06af9
cideg-dev <cideg-dev@users.noreply.github.com>
2025-12-05 06:15:29 +0000

    deploy: c6334192064154e48d4ba21ceb4a2d173014d461

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1638423379"
+    serviceWorkerVersion: "1229472217"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "620a3e4f56c83185ade20b1b918585a2",
-"flutter_bootstrap.js": "1b0882f2698d0b9d6b2aa414aa14245b",
+"flutter_bootstrap.js": "fc63d43b4debb866d0a3e742f764ffd5",
 "version.json": "41d6c5261eae8e8fc247a1521d0f2010",
-"main.dart.js": "05067ce5559d584ccaca525207597cd6",
+"main.dart.js": "9717cd2d566218fecb78a8d85762d37b",
 "landing_page.html": "40b2ab43679acb890795aacd95fc4f36"};
 // The application shell files that are downloaded before a service worker can
 // start.
