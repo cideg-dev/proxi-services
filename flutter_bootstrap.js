8c26cb2cdec7776ecac3e2cb51c364fee631b192
cideg-dev <cideg.p.line@gmail.com>
2025-12-08 22:07:43 +0100

    Fix: Utilisation de CanvasKit en local pour résoudre les erreurs de chargement

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,15 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
+_flutter.config = {
+  canvasKitBaseUrl: "canvaskit/"  // Utiliser CanvasKit localement
+};
+
 _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c4b","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
     serviceWorkerVersion: "2210861806"
-  }
+  },
+  config: _flutter.config
 });
